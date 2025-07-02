699cae25749c85b0096a64009d61153c27979f68
DeBrie <debrie@jrallen.me>
2025-07-02 21:31:04 +0100

    chore: updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: '2048 Hexagon',
+  description: 'Join tiles with the same number to reach 2048!',
 }
 
 export default function RootLayout({
